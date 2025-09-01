a68c94436bd8a5f13d2e6c9011f47dfb664f89a0
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-09-01 16:20:29 -0600

    AgAgregue la task 6

--- task6.js
+++ task6.js
@@ -1,5 +1,24 @@
-// Refer to Task 6 in your Instructions to complete this task
+const prompt = require("prompt-sync")();
+let lineas = prompt("Ingresa hasta que numero quieres que se haga el juego:")
+let lista=[];
+for (let i = 1; i <= lineas; i++) {
+  let output ='';
+  
+  if (i%3===0) {
+    output += 'Fizz'}
+  if (i%5===0){
+    output+= "Buzz"}
+  if(i%7===0){
+    output+="Woof"
+  }
+  if(output !== ""){
+    lista.push(output)
+    
+  } else{
+    lista.push(i)
+    
+  }
+  
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
   };
+console.log(lista);
